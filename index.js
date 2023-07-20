0c37f01a3b9c0d452d85a1f68f50aa4bb3185a3f
afaq ahmad <afaqahmad1324@gmail.com>
2023-07-20 12:39:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    document.getElementById("hidden_menu").classList.toggle("show");
+    document.getElementById("dropbtn").classList.toggle("dropdown_button_background");
+  }
+
+function myFunction2() {
+    document.getElementById("menu_container").classList.toggle("show_menu");
+  }
+
+  // var btn = $('#button');
+
+  // $(window).scroll(function() {
+  //   if ($(window).scrollTop() > 300) {
+  //     btn.addClass('shows');
+  //   } else {
+  //     btn.removeClass('shows');
+  //   }
+  // });
+  
+  // btn.on('click', function(e) {
+  //   e.preventDefault();
+  //   $('html, body').animate({scrollTop:0}, '300');
+  // });
+  
+  
